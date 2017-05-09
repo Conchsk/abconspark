3bc466f5f389a7f25f54bd05d7d5e5b7e3d25c77
Conchsk <Conchsk@outlook.com>
2017-05-09 10:47:12 +0800

    wyj

--- src/main/java/cn/wyj/abconspark/KDDHive.java
+++ src/main/java/cn/wyj/abconspark/KDDHive.java
@@ -1,8 +1,15 @@
 package cn.wyj.abconspark;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.regression.LabeledPoint;
+
 @SuppressWarnings("serial")
 public class KDDHive implements java.io.Serializable {
-	public double[][] data;
+	public List<LabeledPoint> data;
 	public int classNum = 3;
 	public int featureNum = 4;
 	public int sampleNum = 150;
@@ -13,7 +20,6 @@ public class KDDHive implements java.io.Serializable {
 	public int onlookerNum;
 	public int scoutNum;
 
-	public KDDBee[] bees;
 	public double[][] bestMemory;
 	public double bestFitness;
 	
@@ -40,10 +46,92 @@ public class KDDHive implements java.io.Serializable {
 		for (int i = 0; i < sampleNum; ++i) {
 			double tempFit = 0.0;
 			for (int j = 0; j < featureNum; ++j)
-				tempFit += Math.pow(data[i][j] - centers[labeleds[i]][j], 2.0);
+				tempFit += Math.pow(data.get(i).features().apply(j) - centers[labeleds[i]][j], 2.0);
 			fitness += Math.sqrt(tempFit);
 		}
 		return 1 / fitness;
+	}
+	
+	public void solve(JavaSparkContext sc) {
+		JavaRDD<KDDBee> rdd = sc.parallelize(Arrays.asList(new KDDBee[employeNum]));
+		rdd = rdd.map(value -> {
+			double[][] randMem = genRandom();
+			double randFit = calcFitness(randMem);
+			return new KDDBee(randMem, randFit, 0);
+		}).cache();
+		
+		for (int i = 0; i < maxCycle; ++i) {
+			// final var
+			@SuppressWarnings("unchecked")
+			JavaRDD<KDDBee>[] finalVar = new JavaRDD[1];
+			
+			// employe bees
+			finalVar[0] = rdd;
+			JavaRDD<KDDBee> tempRdd = rdd.map(value -> {
+				if (value.trial < trialLimit) {
+					double[][] neighborMem = genNeighbor(value.memory, finalVar[0].takeSample(false, 1).get(0).memory);
+					double neighborFit = calcFitness(neighborMem);
+					if (value.fitness < neighborFit) {
+						value.memory = neighborMem;
+						value.fitness = neighborFit;
+						value.trial = 0;
+					} else
+						++value.trial;
+				} else {
+					value.memory = genRandom();
+					value.fitness = calcFitness(value.memory);
+					value.trial = 0;
+				}
+				return value;
+			}).cache();
+			rdd.unpersist();
+			
+			// onlooker bees
+			double sumOfFitness = rdd.reduce((a, b) -> {
+				a.fitness += b.fitness;
+				return a;
+			}).fitness;
+			
+			finalVar[0] = tempRdd;
+			rdd = tempRdd.map(value -> {
+				for (int j = 0; j < onlookerNum * value.fitness / sumOfFitness; ++j) {
+					double[][] neighborMem = genNeighbor(value.memory, finalVar[0].takeSample(false, 1).get(0).memory);
+					double neighborFit = calcFitness(neighborMem);
+					if (value.fitness < neighborFit) {
+						value.memory = neighborMem;
+						value.fitness = neighborFit;
+						value.trial = 0;
+					} else
+						++value.trial;
+				}
+				return value;
+			}).cache();
+			tempRdd.unpersist();
+			
+			KDDBee bestBee = rdd.reduce((a, b) -> {
+				return a.fitness > b.fitness ? a : b;
+			});
+			if (bestFitness < bestBee.fitness) {
+				bestMemory = bestBee.memory;
+				bestFitness = bestBee.fitness;
+			}
+			
+			// scout
+			tempRdd = sc.parallelize(Arrays.asList(new KDDBee[scoutNum]));
+			tempRdd = tempRdd.map(value -> {
+				double[][] randMem = genRandom();
+				double randFit = calcFitness(randMem);
+				return new KDDBee(randMem, randFit, 0);
+			}).cache();
+			bestBee = tempRdd.reduce((a, b) -> {
+				return a.fitness > b.fitness ? a : b;
+			});
+			if (bestFitness < bestBee.fitness) {
+				bestMemory = bestBee.memory;
+				bestFitness = bestBee.fitness;
+			}
+			tempRdd.unpersist();
+		}
 	}
 	
 	private int[] markLabel(double[][] centers) {
@@ -53,7 +141,7 @@ public class KDDHive implements java.io.Serializable {
 			for (int j = 0; j < classNum; ++j) {
 				double temp = 0;
 				for (int k = 0; k < featureNum; ++k)
-					temp += Math.pow(data[i][k] - centers[j][k], 2.0);
+					temp += Math.pow(data.get(i).features().apply(k) - centers[j][k], 2.0);
 				if (distance > temp) {
 					distance = temp;
 					labeleds[i] = j;
@@ -75,7 +163,7 @@ public class KDDHive implements java.io.Serializable {
 		
 		for (int i = 0; i < sampleNum; ++i) {
 			for (int j = 0; j < featureNum; ++j)
-				centers[labeleds[i]][j] += data[i][j];
+				centers[labeleds[i]][j] += data.get(i).features().apply(j);
 			++tempSum[labeleds[i]];
 		}		
 		for (int i = 0; i < classNum; ++i)
--- src/main/java/cn/wyj/abconspark/KDDMain.java
+++ src/main/java/cn/wyj/abconspark/KDDMain.java
@@ -1,28 +1,36 @@
 package cn.wyj.abconspark;
 
-import java.util.Arrays;
-
-import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.ml.feature.MinMaxScaler;
+import org.apache.spark.ml.feature.MinMaxScalerModel;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.regression.LabeledPoint;
+import org.apache.spark.mllib.util.MLUtils;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-/**
- * Hello world!
- *
- */
+
 public class KDDMain 
 {
     public static void main( String[] args )
     {
-    	SparkSession ss = SparkSession.builder()
+    	SparkSession spark = SparkSession.builder()
     			.appName("test")
     			.master("spark://J106-WYJ-UBT:7077")
-    			.getOrCreate();    	
-    	Dataset<Row> df = ss.read().format("libsvm").load();
-    	ss.close();
+    			.getOrCreate();
+    	JavaRDD<LabeledPoint> rdd = MLUtils.loadLibSVMFile(spark.sparkContext(),
+    			"hdfs://localhost:9000/user/wyj/kddcup/kddcup.data_8*512_all").toJavaRDD();
+    	Dataset<Row> data = spark.createDataFrame(rdd, LabeledPoint.class);
+    	MinMaxScaler scaler = new MinMaxScaler()
+    			.setInputCol("features")
+    			.setOutputCol("scaledFeatures");
+    	MinMaxScalerModel scalerModel = scaler.fit(data);
+    	Dataset<Row> scaledData = scalerModel.transform(data);
+    	JavaRDD<LabeledPoint> scaledRdd = scaledData.javaRDD().map(
+    			value -> new LabeledPoint(value.getDouble(0), (Vector) value.get(2)));
+    	MLUtils.saveAsLibSVMFile(scaledRdd.rdd(),
+    			"hdfs://localhost:9000/user/wyj/kddcup/kddcup.data_8*512_all_scale");
+    	spark.close();
     }
 }
